48e9239e4b9e47111808c44b55fcb43326a67785
chrisvuong20@gmail.com <chrisvuong20@gmail.com>
2022-09-10 16:21:18 +1000

    add a message for Resly

--- encryptionJavaScript/decrypt64.js
+++ encryptionJavaScript/decrypt64.js
@@ -1,4 +1,10 @@
-
+// please copy the value of resume_64x to a file name "data.en". Or you can replace the directory of value file below to the text file storing the 
+// base64 string
+// then run 
+// node decrypt64.js
+// my resume.pdf will be available in the same directory
+// Cheer,
+// Chris Vuong
 const file = "./data.en";
 const toFile = "./resume.pdf";
 const fs = require("fs");
